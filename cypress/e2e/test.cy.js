916897add09844bb4503db4985b3fb31c71572e9
Adrian K <adrian-krako@wp.pl>
2023-04-02 16:58:19 +0200

    rozwiązanie

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -4,15 +4,10 @@ const LoginPage = new Login();
 
 describe("Test", () => {
   it("Logowanie do LMS", () => {
-    // visit page
     LoginPage.navigate();
-    // check title
     LoginPage.validateLoginInputs();
-    // check inputs
     LoginPage.validateInputs();
-    // check button
     LoginPage.validateLoginButton();
-    // check link
     LoginPage.validateLogOutButton();
   });
 });
